7592abf787c866e8f089df3322e2da06e41743b0
pedrohegi <pedrohegi@gmail.com>
2025-09-11 00:27:28 -0300

    adicionamos um endpoint de teste da aula 02 e um teste para o mesmo

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/exercicio-html', response_class=HTMLResponse)
+def exercicio_aula_02():
+    return """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ from fastapi_zero.app import app
 
 
 def test_root_devr_retornar_ok_e_ola_mundo():
-    """ 
+    """
     Esse teste tem 3 etapas (AAA)
     - A: Arrange - Arranjo
     - A: Act     - Executa a coisa (o SUT)
@@ -19,3 +19,12 @@ def test_root_devr_retornar_ok_e_ola_mundo():
     # assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo!'}
+
+
+def test_exercicio_aula_02():
+    client = TestClient(app)
+
+    response = client.get('/exercicio-html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
